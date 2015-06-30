3a11cfaac95155d1f84a77ada6b32a4330704671
Raphaël Vercruyssen <raphael.vercruyssen@infosupport.com>
2015-06-30 17:43:19 +0200

    initial commit

---
+++ src/EventWebsite/Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventWebsite
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+    }
+}
